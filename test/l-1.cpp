71cb246e3a4a8b1116adf8165fe40c29f088a150
justwasif <wasif.iit.ansari@gmail.com>
2025-09-04 05:18:13 +0530

    feat:diff b/w const and var

---
+++ test/l-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int score;
+    score=99;
+    cout<<"wel\n";
+    cout<<score;
+    int out =100;
+    out=19;
+    const int yo=11;
+    return 0;
+}
